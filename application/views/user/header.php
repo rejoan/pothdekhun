dd541b50af5367e9202c2245a3093aa617529c6d
rejoanul <rejoan.er@gmail.com>
2015-12-05 09:04:51 +0600

    home page design change

--- application/views/user/header.php
+++ application/views/user/header.php
@@ -1,4 +1,4 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -13,16 +13,20 @@
         <title><?php echo $title; ?></title>
 
         <!-- Bootstrap core CSS -->
-        <link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
-        <link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet">
-        <link href="<?php echo base_url('assets/css/bootstrap-select.min.css'); ?>" rel="stylesheet">
-        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-        <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <!--    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>-->
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" >
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>" >
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap-select.min.css'); ?>" >
+        <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/font-awesome.min.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/ionicons.min.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/skins/_all-skins.min.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/plugins/iCheck/flat/blue.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/AdminLTE.min.css');?>">
+        <!--[if lt IE 9]><script src="<?php echo base_url('assets/js/ie8-responsive-file-warning.js'); ?>"></script><![endif]-->
+    <!--    <script src="<?php echo base_url('assets/js/ie-emulation-modes-warning.js'); ?>"></script>-->
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
     </head>
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,64 +1,56 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-<div style="margin-top: 20px;" class="alert alert-info">
-    <p><?php echo $this->lang->line('see_transport'); ?></p>
-</div>
 <div class="row">
-    <div class="col-xs-12 col-md-7 col-md-offset-4">
-        <!-- route info pull form -->
-        <form class="form-horizontal" action="<?php echo $action_pull; ?>" method="post">
-            <div class="form-group">
-                
-                <div class="col-xs-10 col-md-6">
-                    <input type="text" class="form-control" id="fromName" placeholder="<?php echo $this->lang->line('from_push'); ?>" required title="যেখান থেকে  যাবেন সেই জায়গার নাম দিন">
-                </div>
+    <div class="col-xs-12 col-md-7 col-md-offset-3">
+        <div class="box box-poth">
+            <div class="box-header">
+                <?php echo $this->lang->line('see_transport'); ?>
             </div>
-            <div class="form-group">
-                <div class="col-xs-10 col-md-6">
-                    <label>
-                        <?php echo $this->lang->line('from_view'); ?>
-                    </label>
-                </div>
+            <div class="box-body">
+                <!-- route info pull form -->
+                <form action="<?php echo $action_pull; ?>" method="post">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="fromName" placeholder="<?php echo $this->lang->line('from_push'); ?>" required title="যেখান থেকে  যাবেন সেই জায়গার নাম দিন">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <?php echo $this->lang->line('from_view'); ?>
+                        </label>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="toName" placeholder="<?php echo $this->lang->line('to_push'); ?>" required title="যেখানে যাবেন সেই জায়গার নাম দিন">
+                    </div>
+                    <input type="submit" class="btn btn-primary btn-lg btn-warning" value="<?php echo $this->lang->line('see_transport_button'); ?>"/>
+                </form>
             </div>
-            <div class="form-group">
-                <div class="col-xs-10 col-md-6">
-                    <input type="email" class="form-control" id="toName" placeholder="<?php echo $this->lang->line('to_push'); ?>" required title="যেখানে যাবেন সেই জায়গার নাম দিন">
-                </div>
-            </div>
-            <input type="submit" class="btn btn-primary btn-lg btn-warning" value="<?php echo $this->lang->line('see_transport_button'); ?>"/>
-        </form>
+        </div>
     </div>
-
 </div><!--/row-->
 
-<div style="margin-top: 20px;" class="alert alert-info">
-    <h3><?php echo $this->lang->line('add_transport_text'); ?></h3>
-</div>
 
 <div class="row">
-    <div class="col-xs-12 col-md-7 col-md-offset-4">
+    <div class="col-xs-12 col-md-7 col-md-offset-3">
+        <div class="box box-poth">
+            <div class="box-header">
+                <?php echo $this->lang->line('add_transport_text'); ?>
+            </div>
+            <div class="box-body">
+                <!-- get route info from user form -->
+                <form id="provide_poth" action="<?php echo $action_groute; ?>" method="post">
+                    <div class="form-group">
+                        <input id="from_push" type="text" class="form-control" name="from_push" placeholder="<?php echo $this->lang->line('from_push'); ?>" required title="যেখান থেকে  যাবে সেই জায়গার নাম দিন">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <?php echo $this->lang->line('from_view'); ?>
+                        </label>
+                    </div>
+                    <div id="lang_error" class="form-group">
+                        <input id="to_push" type="text" class="form-control" name="to_push" placeholder="<?php echo $this->lang->line('to_push'); ?>" required title="যেখানে  যাবে সেই জায়গার নাম দিন">
+                    </div>
 
-        <!-- get route info from user form -->
-        <form id="provide_poth" class="form-horizontal" action="<?php echo $action_groute; ?>" method="post">
-            <div class="form-group">
-                <div class="col-xs-10 col-md-6">
-                    <input id="from_push" type="text" class="form-control" name="from_push" placeholder="<?php echo $this->lang->line('from_push'); ?>" required title="যেখান থেকে  যাবে সেই জায়গার নাম দিন">
-                </div>
+                    <input type="submit" name="push_route" class="btn btn-primary btn-lg btn-warning" value="<?php echo $this->lang->line('add_transport_button'); ?>"/>
+                </form>
             </div>
-            <div class="form-group">
-                <div class="col-xs-10 col-md-6">
-                    <label>
-                        <?php echo $this->lang->line('from_view'); ?>
-                    </label>
-                </div>
-            </div>
-            <div id="lang_error" class="form-group">
-                <div class="col-xs-10 col-md-6">
-                    <input id="to_push" type="text" class="form-control" name="to_push" placeholder="<?php echo $this->lang->line('to_push'); ?>" required title="যেখানে  যাবে সেই জায়গার নাম দিন">
-                </div>
-            </div>
-
-            <input type="submit" name="push_route" class="btn btn-primary btn-lg btn-warning" value="<?php echo $this->lang->line('add_transport_button'); ?>"/>
-        </form>
+        </div>
     </div>
 </div><!--/row-->
