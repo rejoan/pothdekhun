ac64e93a6dd41a1a821ec118ad888cc4cb72d908
rejoanul <rejoan.er@gmail.com>
2015-12-09 18:27:25 +0600

    routes work

--- application/controllers/Routes.php
+++ application/controllers/Routes.php
@@ -13,17 +13,14 @@ class Routes extends CI_Controller {
     }
 
     public function index() {
-        
-
         $this->load->library('pagination');
-
         $config['base_url'] = site_url('routes/index');
         $config['total_rows'] = $this->db->get('routes')->num_rows();
         $config['per_page'] = 10;
         $config['num_links'] = 5;
         $config['full_tag_open'] = '<ul class="pagination no-margin">';
         $config['full_tag_close'] = '</ul>';
-        $config['cur_tag_open'] = '<li class="active"><a href="">';
+        $config['cur_tag_open'] = '<li class="active"><a href="javascript:void();">';
         $config['cur_tag_close'] = '</a></li>';
         $config['prev_tag_open'] = '<li>';
         $config['prev_tag_close'] = '</li>';
@@ -43,11 +40,12 @@ class Routes extends CI_Controller {
             $segment = 0;
         }
         $this->pagination->initialize($config);
-        $query = $this->db->select('r.country,r.from_place,r.to_place,r.type,r.vehicle_name,r.is_verified,r.added,r.is_publish,u.username')->from('routes r')->join('users u', 'r.user_id = u.id', 'left')->get();
+        $query = $this->db->select('r.country,r.from_place,r.to_place,r.type,r.vehicle_name,r.is_verified,r.added,r.is_publish,u.username')->from('routes r')->join('users u', 'r.added_by = u.id', 'left')->get();
         $data = array(
-            'title' => 'All Routes'
+            'title' => 'All Routes',
+            'routes' => $query->result_array()
         );
-        $this->nut_bolts->view_admin('routes', $data, FALSE, FALSE);
+        $this->nut_bolts->view_admin('routes', $data, TRUE, FALSE);
     }
 
 }
--- application/views/admin/routes.php
+++ application/views/admin/routes.php
@@ -1,8 +1,30 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="col-xs-12">
+    <div class="box box-info">
+        <div class="box-header">
+            <h3 class="box-title">Responsive Hover Table</h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body table-responsive no-padding">
+            <table class="table table-hover table-bordered table-striped">
+                <tr>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Transport</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+                <?php foreach($routes as $r):?>
+                <tr>
+                    <td><?php echo $r['from_place'];?></td>
+                    <td><?php echo $r['to_place'];?></td>
+                    <td><?php echo $r['type'];?></td>
+                    <td><span class="label label-success">Approved</span></td>
+                    <td>ds</td>
+                </tr>
+                <?php endforeach;?>
+            </table>
+        </div>
+        <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+</div>
