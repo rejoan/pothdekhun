e87113e1192156165f228b2aafd6993b22c89c66
rejoanul <rejoan.er@gmail.com>
2016-02-24 23:09:11 +0600

    lang loaded

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'dashboard';
 $route['404_override'] = '';
-//$route['^[a-zA-Z]{2}/(.+)$'] = '$1';
+$route['^[a-zA-Z]{2}/(.+)$'] = '$1';
 $route['translate_uri_dashes'] = FALSE;
