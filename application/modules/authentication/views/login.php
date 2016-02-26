743d66392bf9c29e0678620ef72be57ba09b97cb
rejoanul <rejoan.er@gmail.com>
2016-02-26 22:24:20 +0600

    login move from users to authentication

--- application/modules/authentication/views/login.php
+++ application/modules/authentication/views/login.php
@@ -1,134 +1,48 @@
-<!DOCTYPE html>
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<div class="row">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-md-offset-4">
+        <div class="login-box">
+            <div class="login-box-body">
+                <p class="login-box-msg">
+                    <?php
+                    if ($this->session->from_login) {
+                        $f_login = '<strong>' . $this->session->from_login . '</strong> ' . lang('from_view') . ' <strong> ' . $this->session->to_login . ' </strong>';
+                    } else {
+                        $f_login = '';
+                    }
+                    echo $f_login . lang('login_first');
+                    ?>
 
-<html lang="en">
-    <!--<![endif]-->
-    <!-- BEGIN HEAD -->
-    <head>
-        <meta charset="utf-8"/>
-        <title>Login</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-        <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-        <meta content="" name="description"/>
-        <meta content="" name="author"/>
-        <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo base_url('assets/plugins/font-awesome/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo base_url('assets/plugins/simple-line-icons/simple-line-icons.min.css') ?>" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo base_url('assets/plugins/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo base_url('assets/plugins/uniform/css/uniform.default.css') ?>" rel="stylesheet" type="text/css"/>
-        <!-- END GLOBAL MANDATORY STYLES -->
-        <!-- BEGIN PAGE LEVEL STYLES -->
-        <link href="<?php echo base_url('assets/pages/css/login.css') ?>" rel="stylesheet" type="text/css"/>
-        <!-- END PAGE LEVEL SCRIPTS -->
-        <!-- BEGIN THEME STYLES -->
-        <link href="<?php echo base_url('assets/css/components.css') ?>" id="style_components" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo base_url('assets/css/plugins.css') ?>" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo base_url('assets/layout/css/layout.css') ?>" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo base_url('assets/layout/css/themes/darkblue.css') ?>" rel="stylesheet" type="text/css" id="style_color"/>
-        <link href="<?php echo base_url('assets/layout/css/custom.css') ?>" rel="stylesheet" type="text/css"/>
-        <!-- END THEME STYLES -->
-        <link rel="shortcut icon" href="favicon.ico"/>
-    </head>
-    <!-- END HEAD -->
-    <!-- BEGIN BODY -->
-    <body class="login">
-        <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
-        <div class="menu-toggler sidebar-toggler">
-        </div>
-        <!-- END SIDEBAR TOGGLER BUTTON -->
-        <!-- BEGIN LOGO -->
-        <div class="logo">
-            <a href="<?php echo base_url('dashboard'); ?>">
-                <img src="<?php echo base_url('assets/img/logo.png') ?>" alt=""/>
-            </a>
-        </div>
-        <!-- END LOGO -->
-        <!-- BEGIN LOGIN -->
-        <div class="content">
-            <!-- BEGIN LOGIN FORM -->
-            <form class="login-form" action="<?php echo site_url('authentication/login'); ?>" method="post">
-                <h3 class="form-title">Admin Login</h3>
-                <?php if ($this->session->flashdata('message')) { ?>
-                    <div class="alert alert-danger"><?php echo $this->session->flashdata('message'); ?></div>
-                <?php } ?>
-                <?php if (isset($error)): ?>
-                    <p class="alert alert-danger alert-dismissible fade in"><?php echo $error; ?> <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button></p>
-                <?php endif; ?>
-                <div class="form-group">
-                    <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-                    <label class="control-label visible-ie8 visible-ie9">Username</label>
-                    <input class="form-control form-control-solid placeholder-no-fix" type="text" required value="<?php echo set_value('USER_USERNAME'); ?>" autocomplete="off" placeholder="Username" name="USER_USERNAME"/>
-                </div>
-                <div class="form-group">
-                    <label class="control-label visible-ie8 visible-ie9">Password</label>
-                    <input class="form-control form-control-solid placeholder-no-fix" type="password" placeholder="Password" name="USER_PASSWORD"/>
-                </div>
-                <div class="form-actions">
-                    <input name="submit" type="submit" class="btn btn-success uppercase" value="Login">
-                    <label class="rememberme check">
-                        <input type="checkbox" name="remember" value="1"/>Remember </label>
-                    <a href="javascript:;" id="forget-password" class="forget-password">Forgot Password?</a>
-                </div>
-
-                <div class="create-account">
-                    <p>
-                        <a href="javascript:;" id="register-btn" class="uppercase">Create an account</a>
-                    </p>
-                </div>
-            </form>
-            <!-- END LOGIN FORM -->
-            <!-- BEGIN FORGOT PASSWORD FORM -->
-            <form class="forget-form" action="index.html" method="post">
-                <h3>Forget Password ?</h3>
-                <p>
-                    Enter your e-mail address below to reset your password.
                 </p>
-                <div class="form-group">
-                    <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Email" name="email"/>
+
+                <form action="<?php echo $action; ?>" method="post">
+                    <div class="form-group has-feedback">
+                        <input type="email" class="form-control" placeholder="<?php echo lang('email');?>" name="email" required title="আপনার ইমেইল ঠিকানা দিন">
+                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    </div>
+
+                    <div class="form-group has-feedback">
+                        <input type="password" class="form-control" placeholder="<?php echo lang('password');?>" name="password" required title="পাসওয়ার্ড দিন">
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="submit" class="btn btn-primary btn-lg btn-info" value="<?php echo lang('m_login');?>"/>
+                    </div>
+
+                </form>
+
+                <div class="social-auth-links text-center">
+                    <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> <?php echo lang('facebook_login'); ?></a>
+                    <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> <?php echo lang('google_login'); ?></a>
                 </div>
-                <div class="form-actions">
-                    <button type="button" id="back-btn" class="btn btn-default">Back</button>
-                    <button type="submit" class="btn btn-success uppercase pull-right">Submit</button>
-                </div>
-            </form>
-            <!-- END FORGOT PASSWORD FORM -->
+                <!-- /.social-auth-links -->
+
+                <a href="#"><?php echo lang('forgot_pass'); ?></a><br>
+                <a href="<?php echo site_url('users/register'); ?>" class="text-center"><?php echo lang('register_link'); ?></a>
+
+            </div>
+            <!-- /.login-box-body -->
         </div>
-        <div class="copyright">
-            2015 &COPY; ICT-Admin.
-        </div>
-        <!-- END LOGIN -->
-        <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
-        <!-- BEGIN CORE PLUGINS -->
-        <!--[if lt IE 9]>
-        <script src="../../assets/global/plugins/respond.min.js"></script>
-        <script src="../../assets/global/plugins/excanvas.min.js"></script> 
-        <![endif]-->
-        <script src="<?php echo base_url('assets/plugins/jquery.min.js') ?>" type="text/javascript"></script>
-        <script src="<?php echo base_url('assets/plugins/jquery-migrate.min.js') ?>" type="text/javascript"></script>
-        <script src="<?php echo base_url('assets/plugins/bootstrap/js/bootstrap.min.js') ?>" type="text/javascript"></script>
-        <script src="<?php echo base_url('assets/plugins/jquery.blockui.min.js') ?>" type="text/javascript"></script>
-        <script src="<?php echo base_url('assets/plugins/jquery.cokie.min.js') ?>" type="text/javascript"></script>
-        <script src="<?php echo base_url('assets/plugins/uniform/jquery.uniform.min.js') ?>" type="text/javascript"></script>
-        <!-- END CORE PLUGINS -->
-        <!-- BEGIN PAGE LEVEL PLUGINS -->
-        <script src="<?php echo base_url('assets/plugins/jquery-validation/js/jquery.validate.min.js') ?>" type="text/javascript"></script>
-        <!-- END PAGE LEVEL PLUGINS -->
-        <!-- BEGIN PAGE LEVEL SCRIPTS -->
-        <script src="<?php echo base_url('assets/scripts/metronic.js') ?>" type="text/javascript"></script>
-        <script src="<?php echo base_url('assets/layout/scripts/layout.js') ?>" type="text/javascript"></script>
-        <script src="<?php echo base_url('assets/layout/scripts/demo.js') ?>" type="text/javascript"></script>
-        <script src="<?php echo base_url('assets/scripts/login.js') ?>" type="text/javascript"></script>
-        <!-- END PAGE LEVEL SCRIPTS -->
-        <script>
-            jQuery(document).ready(function () {
-                Metronic.init(); // init metronic core components
-                Layout.init(); // init current layout
-                Login.init();
-                Demo.init();
-            });
-        </script>
-        <!-- END JAVASCRIPTS -->
-    </body>
-    <!-- END BODY -->
-</html>
+        <!-- route info pull form -->
+    </div>
+</div><!--/row-->
