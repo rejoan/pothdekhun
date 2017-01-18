66688b610654588db4ab92f6bcb0f1197ab16030
rejoanul <rejoan.er@gmail.com>
2017-01-18 10:43:26 +0600

    transport done

--- application/language/english/transports_lang.php
+++ application/language/english/transports_lang.php
@@ -4,4 +4,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['transport'] = 'All Transport Route Information';
 $lang['tarnsport_picture'] = 'Transport Image';
 $lang['picture_help'] = 'Please provide this transport\'s image';
-$lang['transport_name'] = 'Transport Name';
+$lang['poribohon_info'] = 'Transports details Information';
