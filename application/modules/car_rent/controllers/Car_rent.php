4999056c53791556b90d89774393a51971827ec7
rejoanul <rejoan.er@gmail.com>
2017-01-20 22:53:04 +0600

    rent car module initialized

---
+++ application/modules/car_rent/controllers/Car_rent.php
@@ -0,0 +1,75 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *
+ */
+class Car_rent extends MX_Controller {
+
+    private $user_id = 4;
+
+    public function __construct() {
+        parent::__construct();
+        if ($this->session->user_id) {
+            $this->user_id = $this->session->user_id;
+        }
+        $this->load->model('Comment_model', 'cm');
+    }
+
+    public function index() {
+
+        $data = array(
+            'title' => lang('index')
+        );
+        $this->nl->view_loader('user', 'index', NULL, $data, 'latest', 'rightbar');
+    }
+
+    public function add() {
+        $this->load->library('form_validation');
+        if ($this->input->post('submit')) {
+            $this->form_validation->set_rules('comment', 'Comment', 'required');
+
+            $this->load->library('encryption');
+            $this->encryption->initialize(
+                    array(
+                        'cipher' => 'des',
+                        'mode' => 'ECB'
+                    )
+            );
+            $route_id = $this->encryption->decrypt($this->input->post('pd_identity'));
+            if ($this->form_validation->run() == FALSE) {
+                $this->session->set_flashdata('message', lang('comment_required'));
+                redirect_tr('routes/show/' . $route_id);
+            }
+            $total_comments = $this->pm->total_item('comments', 'route_id', $route_id);
+            if ($total_comments == 0) {// if no comment then left
+                $position = 'left';
+            } elseif ($total_comments % 2 == 0) {// if even then right
+                $position = 'left';
+            } else {
+                $position = 'right';
+            }
+            $comment_data = array(
+                'route_id' => $route_id,
+                'comment' => trim($this->input->post('comment', TRUE)),
+                'user_id' => $this->session->user_id,
+                'position' => $position
+            );
+            $this->db->set('added', 'NOW()', FALSE);
+            $this->pm->insert_data('comments', $comment_data);
+            $this->session->set_flashdata('message', lang('comment_added'));
+            //var_dump($route_id);return;
+            redirect_tr('routes/show/' . $route_id);
+        }
+        redirect_tr('routes');
+    }
+
+    public function hire() {
+        $data = array(
+            'title' => lang('index')
+        );
+        $this->nl->view_loader('user', 'hire', NULL, $data, 'latest', 'rightbar');
+    }
+
+}
