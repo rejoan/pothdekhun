b09dc32c438c637981604a5a78eee1ac7b87da5f
rejoanul <rejoan.er@gmail.com>
2017-01-30 19:14:56 +0600

    contact us

--- application/modules/pages/controllers/Pages.php
+++ application/modules/pages/controllers/Pages.php
@@ -25,7 +25,36 @@ class Pages extends MX_Controller {
             'title' => lang('contact_us'),
             'settings' => $this->nl->get_config(),
         );
-        
+        $this->load->library('form_validation');
+
+        if ($this->input->post('submit')) {
+            $this->form_validation->set_rules('name', 'Name', 'required');
+            $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+            $this->form_validation->set_rules('comment', 'Comment', 'required');
+            if ($this->form_validation->run() == FALSE) {
+                $this->nl->view_loader('user', 'contact', NULL, $data, 'latest', 'rightbar');
+                return;
+            }
+            $from_mail = trim($this->input->post('email', TRUE));
+            $comment = trim($this->input->post('comment', TRUE));
+            $this->load->library('email');
+            $subject = 'Pothdekhun contact';
+            $body = '<p>Comment: ' . $comment . '</p>';
+            $config['mailtype'] = 'html';
+            $config['protocol'] = 'sendmail';
+            $this->email->initialize($config);
+            $this->email->from($from_mail, 'PothDekhun');
+            $this->email->to('rejoan.er@gmail.com');
+            $this->email->subject($subject);
+            $this->email->message($body);
+
+            if ($this->email->send()) {
+                $this->session->set_flashdata('message', lang('mail_sent'));
+                redirect_tr('pages/contact_us');
+            }else{
+                //echo $this->email->print_debugger();
+            }
+        }
         $this->nl->view_loader('user', 'contact', NULL, $data, 'latest', 'rightbar');
     }
 
--- application/modules/pages/views/contact.php
+++ application/modules/pages/views/contact.php
@@ -1,11 +1,17 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <div class="col-xs-12 col-md-6">
+    <?php
+    $message = $this->session->flashdata('message');
+    if ($message) {
+        echo '<div class="alert alert-warning">' . $message . '</div>';
+    }
+    ?>
     <div class="box box-poth">
         <div class="box-header">
             <h3 class="box-title"><?php echo lang('contact_us'); ?></h3>
         </div>
         <div class="box-body">
-            <form class="form-horizontal" action="<?php echo site_url('pages/contact'); ?>" method="POST" enctype="multipart/form-data">
+            <form class="form-horizontal" action="<?php echo site_url('pages/contact_us'); ?>" method="POST" enctype="multipart/form-data">
                 <div class="form-group">
                     <label for="inputName" class="col-sm-3 control-label"><?php echo lang('name'); ?></label>
 
@@ -13,6 +19,7 @@
                         <input type="text" class="form-control" name="name" placeholder="<?php echo lang('name'); ?>">
                     </div>
                 </div>
+                <?php echo form_error('name', '<div class="alert alert-danger">', '</div>'); ?>
                 <div class="form-group">
                     <label class="col-sm-3 control-label"><?php echo lang('email'); ?></label>
 
@@ -20,14 +27,15 @@
                         <input type="text" class="form-control" name="email" placeholder="<?php echo lang('email'); ?>">
                     </div>
                 </div>
-                 <div class="form-group">
+                <?php echo form_error('email', '<div class="alert alert-danger">', '</div>'); ?>
+                <div class="form-group">
                     <label class="col-sm-3 control-label"><?php echo lang('comment'); ?></label>
 
                     <div class="col-sm-9">
                         <textarea class="form-control" name="comment" placeholder="<?php echo lang('comment'); ?>"></textarea>
                     </div>
                 </div>
-
+                <?php echo form_error('comment', '<div class="alert alert-danger">', '</div>'); ?>
                 <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-10">
                         <input name="submit" type="submit" class="btn btn-info" value="<?php echo lang('send'); ?>">
