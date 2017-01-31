9681624112ea9243e2e959d37c4581e225c26c46
rejoanul <rejoan.er@gmail.com>
2017-01-31 14:22:48 +0600

    comment fix

--- application/modules/comments/views/index.php
+++ application/modules/comments/views/index.php
@@ -23,17 +23,17 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($comments as $r):$segment++; ?>
+                    <?php foreach ($comments as $r): ?>
                         <tr>
-                            <th><?php echo $segment; ?></th>
+                            <th><?php echo $r['id']; ?></th>
                             <td><?php echo empty($r['username']) ? $r['email'] : $r['username']; ?></td>
                             <td><?php echo $r['comment']; ?></td>
                             <td><a href="<?php echo site_url('routes/show/').$r['route_id'];?>"></a></td>
-                            <td><?php echo $this->nl->date_formatter('Y-m-d H:i:s',$r['added'],'m D, y'); ?></td>
+                            <td><?php echo $this->nl->date_formatter('Y-m-d H:i:s',$r['added'],'d M, y'); ?></td>
                             <td><?php echo $r['username']; ?></td>
 
                             <td>
-                                <a class="btn btn-info" target="_blank" data-toggle="tooltip" data-placement="top" title="Update" href="<?php echo site_url('comments/delete') . '/' . $r['id']; ?>"><i class="fa fa-link"></i></a>
+                                <a onclick="return confirm('Delete this Comment?');" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Update" href="<?php echo site_url('comments/delete') . '/' . $r['id']; ?>"><i class="fa fa-trash"></i></a>
                                 
                             </td>
                         </tr>
